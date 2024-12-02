e5eea1b877f4ec30aa232d517cc4d8065ad9ee40
AdhamSamehh <an2300132@tkh.edu.eg>
2024-12-02 15:02:17 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+// const db_access = require('./db.js');
+// const db = db_access.db;
+const server = express();
+const port = 8888;
+server.use(express.json());
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./database.db');
+
+
+
+//DATABASE TABLES
+// Creating User table
+const createusertable = `
+  CREATE TABLE IF NOT EXISTS user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL, 
+    email TEXT UNIQUE NOT NULL, 
+    password TEXT NOT NULL,
+    age INTEGER
+    )`;
+
+
+
+server.post('/user/register', (req, res) => {
+    let name = req.body.name;
+    let email = req.body.email;
+    let password = req.body.password;
+    let age = req.body.age;
+    if (!name || !email || !password) {
+        return res.status(400).send("name, email, and password are required.");
+    }
+    const insertquery = `INSERT INTO USER(name, email, password) Values('${name}', '${email}', '${password}')`;
+    db.run(insertquery, (err) => {
+        if (err) {
+            return res.status(500).send(`Error during registration: ${ err.message }`);
+        } else {
+            return res.status(200).send("Registration successful");
+        }
+    })
+})
+
+
+
+db.serialize(() => {
+    db.exec(createusertable, (err) => {
+        if (err) {
+            console.error("Error creating user table:", err);
+        } else {
+            console.log("User table created successfully!");
+        }
+    })
+})
+
+
+
+server.listen(port, () => {
+    console.log(`Server is listening at port ${ port }`)
+});
